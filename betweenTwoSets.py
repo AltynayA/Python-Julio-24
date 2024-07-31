b89ac63814a44e510d6beda377b8befbc1e0e213
altynay <goldmoon321123@gmail.com>
2024-07-31 06:44:51 +0500

    Finding GCD and LCM and https://www.hackerrank.com/challenges/between-two-sets/problem

---
+++ betweenTwoSets.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from functools import reduce
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    gcd = reduce(math.gcd, b)
+    lcm = reduce(math.lcm, a)
+    count = 0
+    if gcd % lcm == 0:
+        for x in range(a[0], b[0] +1):
+            if x % lcm == 0 and gcd % x == 0:
+                count+=1
+    return count
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
+    print(total)
